843e8ee4e8e347aa60f542cbf2056ea4081fe630
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2025-07-22 14:34:58 +0300

    add error fadeout animation

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,14 +1,22 @@
 import { createPortal } from 'react-dom';
 import css from './Error.module.css';
+import { useEffect, useState } from 'react';
 
 interface ErrorProps {
   error: Error | string;
 }
 
 export default function Error({ error }: ErrorProps) {
+  const [fade, setFade] = useState(false);
+
+  useEffect(() => {
+    const fadeTimer = setTimeout(() => setFade(true), 2500);
+    return () => clearTimeout(fadeTimer);
+  }, []);
+
   const message = typeof error === 'string' ? error : error.message;
   return createPortal(
-    <div className={css.container}>
+    <div className={`${css.container} ${fade ? css.fadeOut : ''}`}>
       <strong>Error:</strong> {message || 'Something went wrong'}
     </div>,
     document.getElementById('modal-root') as HTMLDivElement
